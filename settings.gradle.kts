29cb0d4a9ac4dca67e618f056b2c67381f3c51e9
AuraKai <wehttam1989@gmail.com>
2025-09-20 11:18:26 -0600

    Add foojay-resolver plugin for JDK management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,12 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
 // settings.gradle.kts
 
-// settings.gradle.kts
+
+plugins {
+    // Apply the foojay-resolver plugin to allow automatic download of JDKs.
+    // Use the latest stable version.
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" 
+}
 
 
 pluginManagement {
